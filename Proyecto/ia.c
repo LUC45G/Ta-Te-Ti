afb97948d56ade3bba89927f432f5aea0e82fab1
LUC45G <lucasgxnzalez@gmail.com>
2019-10-30 20:53:38 -0300

    ia casi terminada

--- Proyecto/ia.c
+++ Proyecto/ia.c
@@ -50,7 +50,56 @@ void crear_busqueda_adversaria(tBusquedaAdversaria * b, tPartida p){
 /**
 >>>>>  A IMPLEMENTAR   <<<<<
 */
-void proximo_movimiento(tBusquedaAdversaria b, int * x, int * y){}
+void proximo_movimiento(tBusquedaAdversaria b, int * x, int * y){
+
+    tNodo raiz                  = a_raiz(b->arbol_busqueda);
+    tLista sucesores            = a_hijos(b->arbol_busqueda, raiz);
+    int cantidad                = l_longitud(sucesores);
+    tPosicion posListaSucesores = l_primera(sucesores);
+    tNodo nodoSucesor           = l_recuperar(sucesores, posListaSucesores);
+    tEstado estadoSucesor       = a_recuperar(b->arbol_busqueda, nodoSucesor);
+    tEstado estadoInicial       = a_recuperar(b->arbol_busqueda, raiz);
+    tEstado estadoGana          = NULL;
+    tEstado estadoEmpata        = NULL;
+    tEstado estadoPierde        = NULL;
+    tEstado estadoReturn        = NULL;
+
+    while(cantidad > 0 && estadoGana == NULL) {
+        cantidad--;
+
+        if (estadoSucesor->utilidad == IA_GANA_MAX) {
+            estadoGana   = estadoSucesor;
+        }
+
+        if (estadoSucesor->utilidad == IA_EMPATA_MAX) {
+            estadoEmpata = estadoSucesor;
+        }
+
+        if (estadoSucesor->utilidad == IA_PIERDE_MAX) {
+            estadoPierde = estadoSucesor;
+        }
+
+        if (cantidad > 0) {
+            tPosicion posListaSucesores = l_siguiente(sucesores, posListaSucesores);
+            tNodo nodoSucesor           = l_recuperar(sucesores, posListaSucesores);
+            tEstado estadoSucesor       = a_recuperar(b->arbol_busqueda, nodoSucesor);
+        }
+    }
+
+    if (estadoGana != NULL) {
+        estadoReturn = estadoGana;
+    }
+    else if (estadoEmpata != NULL) {
+        estadoReturn = estadoEmpata;
+    }
+    else {
+        estadoReturn = estadoPierde;
+    }
+
+    diferencia_estados(estadoInicial, estadoReturn, x, y);
+
+
+}
 
 /**
 >>>>>  A IMPLEMENTAR   <<<<<
@@ -95,15 +144,24 @@ Computa el valor de utilidad correspondiente al estado E, y la ficha correspondi
 **/
 static int valor_utilidad(tEstado e, int jugador_max) {
 
-    int i, j;
+    int i, j, min, max;
+
+    if( jugador_max == PART_JUGADOR_1 ) {
+        min = PART_JUGADOR_2;
+        max = PART_JUGADOR_1;
+    }
+    else {
+        min = PART_JUGADOR_1;
+        max = PART_JUGADOR_2;
+    }
 
     // Chequeo por fila
     for(i = 0; i < 3; ++i) {
         if(e->grilla[i][0] == e->grilla[i][1] && e->grilla[i][0] == e->grilla[i][2]) {
-            if(e->grilla[i][0] == PART_JUGADOR_1) {
+            if(e->grilla[i][0] == max) {
                 return IA_GANA_MAX;
             }
-            if(e->grilla[i][0] == PART_JUGADOR_2) {
+            if(e->grilla[i][0] == min) {
                 return IA_PIERDE_MAX;
             }
         }
@@ -112,10 +170,10 @@ static int valor_utilidad(tEstado e, int jugador_max) {
     // Chequeo por columna
     for(i = 0; i < 3; ++i) {
         if(e->grilla[0][i] == e->grilla[i][1] && e->grilla[1][i] == e->grilla[2][i]) {
-            if(e->grilla[0][i] == PART_JUGADOR_1) {
+            if(e->grilla[0][i] == max) {
                 return IA_GANA_MAX;
             }
-            if(e->grilla[0][i] == PART_JUGADOR_2) {
+            if(e->grilla[0][i] == min) {
                 return IA_PIERDE_MAX;
             }
         }
@@ -123,19 +181,19 @@ static int valor_utilidad(tEstado e, int jugador_max) {
 
     // Chequeo por diagonales
     if(e->grilla[0][2] == e->grilla[1][1] && e->grilla[0][2] == e->grilla[2][0]) {
-        if(e->grilla[0][2] == PART_JUGADOR_1) {
+        if(e->grilla[0][2] == max) {
             return IA_GANA_MAX;
         }
-        if(e->grilla[0][2] == PART_JUGADOR_2) {
+        if(e->grilla[0][2] == min) {
             return IA_PIERDE_MAX;
         }
     }
 
     if(e->grilla[0][0] == e->grilla[1][1] && e->grilla[0][0] == e->grilla[2][2]) {
-        if(e->grilla[0][0] == PART_JUGADOR_1) {
+        if(e->grilla[0][0] == max) {
             return IA_GANA_MAX;
         }
-        if(e->grilla[0][0] == PART_JUGADOR_2) {
+        if(e->grilla[0][0] == min) {
             return IA_PIERDE_MAX;
         }
     }
@@ -163,7 +221,33 @@ estados_sucesores(estado, ficha) retornaría dos listas L1 y L2 tal que:
 - L1 y L2 tienen exactamente los mismos estados sucesores de ESTADO a partir de jugar FICHA.
 - El orden de los estado en L1 posiblemente sea diferente al orden de los estados en L2.
 **/
-static tLista estados_sucesores(tEstado e, int ficha_jugador){}
+static tLista estados_sucesores(tEstado e, int ficha_jugador) {
+
+    tLista listaReturn;
+    crear_lista(&listaReturn);
+    int i, j;
+    tEstado sigEstado;
+
+    for(i = 0; i < 3; ++i) {
+        for(j = 0; j < 3; ++j) {
+            if(e->grilla[i][j] == PART_SIN_MOVIMIENTO) {                                // Si el par [i, j] actual esta libre
+                sigEstado = clonar_estado(e);                                           // Clono el estado
+                sigEstado->grilla[i][j] = ficha_jugador;                                // Agrego la ficha pertinente
+                sigEstado->utilidad     = valor_utilidad(sigEstado, ficha_jugador);     // Calculo su valor de utilidad
+
+                if (&sigEstado % 3 == 0) {                                              // Si su puntero es divisible por 3
+                    l_insertar(listaReturn, listaReturn, sigEstado);                    // Lo coloco al principio
+                }
+                else {                                                                  // Sino
+                    l_insertar(listaReturn, l_fin(listaReturn), sigEstado);             // Lo coloco al final
+                }                                                                       // Esto le da la aleatoriedad
+            }
+        }
+    }
+
+    return listaReturn;
+
+}
 
 /**
 >>>>>  A IMPLEMENTAR   <<<<<
@@ -171,7 +255,18 @@ Inicializa y retorna un nuevo estado que resulta de la clonación del estado E.
 Para esto copia en el estado a retornar los valores actuales de la grilla del estado E, como su valor
 de utilidad.
 **/
-static tEstado clonar_estado(tEstado e){}
+static tEstado clonar_estado(tEstado e) {
+    tEstado newE = malloc(sizeof(struct estado));
+    int i, j;
+
+    for(i = 0; i < 3; ++i) {
+        for(j = 0; j < 3; ++j) {
+            newE->grilla[i][j] = e->grilla[i][j]; // Esto me genera dudas, porque en java funcionaria pero esto no es java
+        }
+    }
+
+    return newE;
+}
 
 /**
 Computa la diferencia existente entre dos estados.
